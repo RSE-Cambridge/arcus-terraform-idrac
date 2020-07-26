cb7a6053b227db13ce76e37b63ebbb843a44b07e
Paul Browne <pfb29@cam.ac.uk>
2020-07-27 00:36:12 +0100

    Populate Ironic node extra data with pre-determined pxe_bootstrap_ip

--- python/ironic_enrol.py
+++ python/ironic_enrol.py
@@ -4,15 +4,16 @@ import pprint
 import sys
 
 import openstack
-openstack.enable_logging(True, stream=sys.stdout)
+#openstack.enable_logging(True, stream=sys.stdout)
 
 def find_idrac_ports(conn):
     return [{
             "name": raw_port["name"],
             "mac": raw_port["mac_address"],
             "ip": raw_port["fixed_ips"][0]["ip_address"],
-            "rack": [tag for tag in raw_port["tags"] if "DR" in tag][0],
+            "rack": [tag for tag in raw_port["tags"] if "AR" in tag][0],
             "datacentre": [tag for tag in raw_port["tags"] if "DC" in tag][0],
+            "pxe_bootstrap_ip": [tag for tag in raw_port["tags"] if "10.45" in tag][0],
         }
         for raw_port in conn.network.ports(tags="iDRAC")]
 
@@ -20,23 +21,30 @@ def find_idrac_ports(conn):
 def find_baremetal_servers(conn):
     return list(conn.baremetal.nodes(details=True))
 
+# Open connection to the OpenStack cloud
 conn = openstack.connection.from_config(cloud="arcus", debug=True)
-idracs = find_idrac_ports(conn)
-print(len(idracs))
 
+# Populate IDRAC port
+idracs = find_idrac_ports(conn)
+print("IDRAC ports found: ", len(idracs))
+
+# Populate list of existing baremetal servers
 baremetal_nodes = find_baremetal_servers(conn)
-print(len(baremetal_nodes))
+print("Total Baremetal nodes found: ",len(baremetal_nodes))
 
 already_created = [node["name"] for node in baremetal_nodes]
 
+# Enroll nodes not already enrolled in Ironic, with predefined set of drivers and metadata
 new_nodes = []
 for idrac in idracs:
     if idrac["name"] not in already_created:
+        # Prepopulate extra field for PXE network IP address
+        pxe_ip = idrac["ip"].replace("45","43")
         new_node = conn.baremetal.create_node(
             name=idrac["name"], driver="idrac",
             driver_info={
               "drac_address": idrac["ip"],
-              # Starting with default passwords as shipped, updates later
+              # Starting with default passwords as shipped, update all later
               "drac_password": "calvin",
               "drac_username": "root",
               "redfish_system_id": "/redfish/v1/Systems/System.Embedded.1",
@@ -58,22 +66,21 @@ for idrac in idracs:
             vendor_interface="idrac-wsman",
             extra={
                "rack": idrac["rack"],
-               "datacentre": idrac["datacentre"]},
+               "datacentre": idrac["datacentre"],
+               "pxe_bootstrap_ip": idrac["pxe_bootstrap_ip"]
+            },
             retry_on_conflict=False,
             provision_state="enroll",
             resource_class="c6420.p8276.m192")
         new_nodes.append(new_node)
-        pprint.pprint(new_node)
 pprint.pprint(new_nodes)
-print(len(new_nodes))
+print("New nodes found: ", len(new_nodes))
 
 if len(new_nodes) > 0:
     baremetal_nodes = find_baremetal_servers(conn)
-print(len(baremetal_nodes))
+print("Total baremetal nodes now: ", len(baremetal_nodes))
 
-#
-# if in enroll state, go to manage state
-#
+# If node is in enroll state, go to manage state
 move_to_manage = []
 for node in baremetal_nodes:
     if node["provision_state"] == "enroll":
@@ -85,9 +92,7 @@ if len(move_to_manage) > 0:
 if len(move_to_manage) > 0:
     baremetal_nodes = find_baremetal_servers(conn)
 
-#
-# doing out of bound inspection, if cpus not populated
-#
+# Perform an out-of-band inspection, if CPU data not populated
 inspecting = []
 for node in baremetal_nodes:
     if node["properties"].get("cpus") is None and node["provision_state"] == "manageable":
@@ -95,23 +100,18 @@ for node in baremetal_nodes:
         inspecting.append(node)
 if len(inspecting) > 0:
     conn.baremetal.wait_for_nodes_provision_state(inspecting, 'manageable')
-
-
 if len(inspecting) > 0:
     baremetal_nodes = find_baremetal_servers(conn)
 
-#
-# Get out of maintanaince mode
-#
-#for node in baremetal_nodes:
+# Get nodes out of maintenance mode
+#for node in new_nodes:
 #    if node["is_maintenance"]:
 #        print("Warning: node was in maintanance mode: " + node['name'])
 #        conn.baremetal.unset_node_maintenance(node)
 
-#
-# power everything off
-#
-target_power = "power off"
-for node in baremetal_nodes:
-    if node["power_state"] != target_power:
-        conn.baremetal.set_node_power_state(node, target_power)
+# Power everything off
+# We should only power newly manageable nodes off (not any pre-existing inventory)
+#target_power = "power off"
+#for node in new_nodes:
+#    if node["power_state"] != target_power:
+#        conn.baremetal.set_node_power_state(node, target_power)
