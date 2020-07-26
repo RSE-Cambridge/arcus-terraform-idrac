c955c8605561a2245e8c61c06ce620d280e14231
Paul Browne <pfb29@cam.ac.uk>
2020-07-27 00:20:25 +0100

    Use fixed IP addressing from the inventory data for BMC Neutron port creation
    Also update field headers to current standards, and inject a fixed IP metadata tag for the eventual equivalent
    PXE bootstrap Neutron port.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,31 +13,41 @@ provider "local" {
 
 # Example entry in parsed csv:
 # [{
-#    "bmc_mac_format" = "6C:2B:59:00:00:00"
-#    "device_name" = "sv1-ab07-u2"
-#    "device_serial" = "AABBCC12"
-#    "management_mac_format" = "6C:2B:59:00:00:01"
-#    "datacentre_id" = "AC-H3"
+#    "bmc_mac" = "6C:2B:59:00:00:00"
+#    "hardware_name" = "svn1-ab07-u2"
+#    "serial" = "AABBCC12"
+#    "mac" = "6C:2B:59:00:00:01"
+#    "dc" = "AC-H3"
 #    "rack" = "AB07"
 #    "rack_pos" = "2"
+#    "pxe_bootstrap_ip" = "10.45.103.1"
 #  }]
+
 locals {
-  idrac_mapping = csvdecode(file("idrac.csv"))
+  idrac_mapping = csvdecode(file("AR04.csv"))
 }
 
-data "openstack_networking_network_v2" "network" {
-  name = "out-of-band-management"
+data "openstack_networking_network_v2" "bmc_network" {
+  name = "WCDC-BMC-45"
+}
+data "openstack_networking_subnet_v2" "bmc_subnet"{
+  name = "WCDC-BMC-45"
 }
 
 resource "openstack_networking_port_v2" "ports" {
   for_each = { for mapping in local.idrac_mapping:
-               mapping.device_name => mapping}
+               mapping.hardware_name => mapping}
 
-  name           = each.value.device_name
-  network_id     = data.openstack_networking_network_v2.network.id
-  mac_address    = each.value.bmc_mac_format
+  name           = each.value.hardware_name
+  network_id     = data.openstack_networking_network_v2.bmc_network.id
+  mac_address    = each.value.bmc_mac
+  fixed_ip {
+      ip_address = each.value.bmc_ip
+      subnet_id  = data.openstack_networking_subnet_v2.bmc_subnet.id
+
+  }
   admin_state_up = "true"
-  tags           = ["iDRAC", each.value.datacentre_id, each.value.rack]
+  tags           = ["iDRAC", each.value.dc, each.value.rack, each.value.pxe_bootstrap_ip]
 }
 
 #output idrac {
